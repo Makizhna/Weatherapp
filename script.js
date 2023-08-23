b416ae9a2a2c8612668f9aba4ad119469ed725fe
Makizhna <134484163+Makizhna@users.noreply.github.com>
2023-08-23 11:56:41 +0530

    Update script.js

--- script.js
+++ script.js
@@ -19,14 +19,3 @@ function getWeather(location) {
 
   weatherInfo.innerHTML = weatherHTML;
 }
-function dateBuilder (d) {
-  let months = ["January", "February", "March", "April", "May", "June", "July", "August", "September", "October", "November", "December"];
-  let days = ["Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"];
-
-  let day = days[d.getDay()];
-  let date = d.getDate();
-  let month = months[d.getMonth()];
-  let year = d.getFullYear();
-
-  return `${day} ${date} ${month} ${year}`;
-}
